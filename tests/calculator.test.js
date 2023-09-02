46ec56291ef541605d0139d3e7c79f67197bd83a
wendellnd <wendelldourado.wn@gmail.com>
2023-09-02 12:44:18 -0300

    feat: add real tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,3 +1,31 @@
-test("1 = 1", () => {
-    expect(1).toBe(1);
+const calculator = require("../models/calculator");
+
+test("Should sum 2+2", () => {
+    const result = calculator.sum(2, 2);
+
+    expect(result).toBe(4);
+});
+
+test("Should sum 5+100", () => {
+    const result = calculator.sum(5, 100);
+
+    expect(result).toBe(105);
+});
+
+test("'banana' + 100 should return an error", () => {
+    const result = calculator.sum("banana", 100);
+
+    expect(result).toBe("Error");
+});
+
+test("100 + 'banana' should return an error", () => {
+    const result = calculator.sum(100, "banana");
+
+    expect(result).toBe("Error");
+});
+
+test("'banana' + 'banana' should return an error", () => {
+    const result = calculator.sum("nanana", "banana");
+
+    expect(result).toBe("Error");
 });
