6fe4b2584dc31e999393156dc8e32aecb9deacbf
WendellND <51250646+wendellnd@users.noreply.github.com>
2024-02-10 19:51:42 -0300

    fix: insert client.connect into try catch

--- infra/database.js
+++ infra/database.js
@@ -1,24 +1,24 @@
 import { Client } from "pg";
 
 const query = async (queryObject) => {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  try {
     await client.connect();
-    try {
-        const result = await client.query(queryObject);
-        return result;
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await client.end();
-    }
+    const result = await client.query(queryObject);
+    return result;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
 };
 
 export default {
-    query,
+  query,
 };
