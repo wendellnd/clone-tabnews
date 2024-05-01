28baaa1e30d6d17454a3e0a232bede2fa6952483
wendellnd <wendelldourado.wn@gmail.com>
2024-05-01 15:59:28 -0300

    feat: add support to ssl in production

--- infra/database.js
+++ infra/database.js
@@ -1,24 +1,26 @@
 import { Client } from "pg";
 
 const query = async (queryObject) => {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.end();
-  }
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        ssl: process.env.NODE_ENV == "production" ? true : false,
+    });
+
+    try {
+        await client.connect();
+        const result = await client.query(queryObject);
+        return result;
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.end();
+    }
 };
 
 export default {
-  query,
+    query,
 };
