83373a3d237c30b32ab4f280e9e498b9319169ea
Wendell Dourado <51250646+wendellnd@users.noreply.github.com>
2025-01-30 18:02:04 -0300

    Lint format actions (#3)
    * add linting github action
    
    * add eslint configs
    
    * adds eslint job
    
    * fix package-lock file
    
    * fix eslint warnings
    
    * fix prettier linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import fetch from "node-fetch";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -19,6 +20,8 @@ async function waitForAllServices() {
   }
 }
 
-export default {
+const orchestrator = {
   waitForAllServices,
 };
+
+export default orchestrator;
